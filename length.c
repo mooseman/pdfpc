b61fe1edab85246b7fe0c7494e9fa34bbdc716e9
Andy Elvey <andy@obsidian.(none)>
2010-02-04 21:41:52 +1300

    Added length.c

---
+++ length.c
@@ -0,0 +1,25 @@
+
+/*  length.c  */  
+
+/*  Get the length of an array  */ 
+/*  This code is released to the public domain  */  
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+int main()
+{
+    int myarray[] = {32, 67, 25, 48}; 
+    int length = sizeof(myarray) / sizeof(*myarray);
+    printf( "myarray at %x has length %d \n", myarray, sizeof(myarray) / sizeof(*myarray) ) ;
+    printf("length of myarray is %d \n", length); 
+    return 0; 
+}
+
+
+
